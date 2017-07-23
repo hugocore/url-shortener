2e003ecc8e32318158977fd15b847edec5805216
Hugo Sequeira <hugoandresequeira@gmail.com>
2017-07-24 00:47:05 +0100

    Add service to resolve links

---
+++ api/app/services/link_resolver.rb
@@ -0,0 +1,24 @@
+require_relative 'service'
+
+module Shortener
+  module Services
+    class LinkResolver
+      extend Service
+
+      def initialize(code:)
+        @code = code
+      end
+
+      def call()
+        @link = Shortener::Models::Link.find_by_code(@code)
+
+        @link.increment!(:clicks)
+
+        @link
+      end
+    end
+  end
+end
+
+
+
