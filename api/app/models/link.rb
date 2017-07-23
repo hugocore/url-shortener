1d0a412f56a7fab636749f304522c36059b8178d
Hugo Sequeira <hugoandresequeira@gmail.com>
2017-07-23 23:06:33 +0100

    Add validations to Link

--- api/app/models/link.rb
+++ api/app/models/link.rb
@@ -1,7 +1,16 @@
 module Shortener
   module Models
     class Link < ActiveRecord::Base
+      RESERVED_CODES = %w(
+        robots sex badwords
+      ).freeze
 
+      validates :code, presence: true
+      validates :code, uniqueness: { message: 'already exists' }
+      validates :code, exclusion: { in: RESERVED_CODES, message: '"%{value}" is not allowed.' }
+
+      validates :url, presence: true
+      validates :url, uniqueness: { message: 'already exists' }
     end
   end
 end
