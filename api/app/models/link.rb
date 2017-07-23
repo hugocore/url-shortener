4fa907957f4bcc3523294aa19c082771907a7f67
Hugo Sequeira <hugoandresequeira@gmail.com>
2017-07-23 20:27:23 +0100

    Add Link model

---
+++ api/app/models/link.rb
@@ -0,0 +1,7 @@
+module Shortener
+  module Models
+    class Link < ActiveRecord::Base
+
+    end
+  end
+end
---
+++ api/db/migrate/20170723182504_create_links.rb
@@ -0,0 +1,12 @@
+class CreateLinks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :links do |t|
+      t.string :code, null: false
+      t.string :url, null: false
+      t.integer :clicks, default: 0
+      t.timestamps
+
+      t.index :code
+    end
+  end
+end
