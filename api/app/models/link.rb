98e7dbe44aee6f5cb3ab4d2389916c13db81590c
Hugo Sequeira <hugoandresequeira@gmail.com>
2017-07-24 00:48:12 +0100

    Include concern to generate Link's codes

--- api/app/models/link.rb
+++ api/app/models/link.rb
@@ -1,16 +1,25 @@
+require_relative '../concerns/unique_codes'
+
 module Shortener
   module Models
     class Link < ActiveRecord::Base
+      include UniqueCodes
+
       RESERVED_CODES = %w(
         robots sex badwords
       ).freeze
 
-      validates :code, presence: true
       validates :code, uniqueness: { message: 'already exists' }
       validates :code, exclusion: { in: RESERVED_CODES, message: '"%{value}" is not allowed.' }
 
       validates :url, presence: true
       validates :url, uniqueness: { message: 'already exists' }
+
+      before_validation :generate_code, on: :create, unless: :code?
+
+      def generate_code
+        self.code = generate_small_code(:code)
+      end
     end
   end
 end
