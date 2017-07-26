df1e1db6f9fb82619be211c7053e4626d0c80f63
Hugo Sequeira <hugoandresequeira@gmail.com>
2017-07-26 21:13:58 +0100

    Add ESLint and missing plugins

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,8 +1,9 @@
 // http://webpack.github.io/docs/configuration.html
 // http://webpack.github.io/docs/webpack-dev-server.html
-var app_root = 'src'; // the app root folder: src, src_users, etc
-var path = require('path');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+var app_root = 'src' // the app root folder: src, src_users, etc
+var path = require('path')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   app_root: app_root, // the app root folder, needed by the other webpack configs
@@ -19,10 +20,19 @@ module.exports = {
     filename: 'bundle.js',
   },
   module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
+    ],
     loaders: [
       {
-        test: /\.js$/,
-        loaders: ['react-hot', 'babel'],
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel', 'eslint-loader'],
         exclude: /node_modules/,
       },
       {
@@ -39,11 +49,16 @@ module.exports = {
   devServer: {
     contentBase: __dirname + '/public',
   },
+  resolve: {
+    root: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')],
+    extensions: ['', '.js', '.jsx']
+  },
   plugins: [
+    new ExtractTextPlugin('css/main.css'),
     new CleanWebpackPlugin(['css/main.css', 'js/bundle.js'], {
       root: __dirname + '/public',
       verbose: true,
       dry: false, // true for simulation
     }),
   ],
-};
+}
--- front-end/webpack.prod.config.js
+++ front-end/webpack.prod.config.js
@@ -1,12 +1,12 @@
-var webpack = require("webpack");
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-module.exports = require('./webpack.config.js');    // inherit from the main config file
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+module.exports = require('./webpack.config.js')    // inherit from the main config file
 
 // disable the hot reload
 module.exports.entry = [
   'babel-polyfill',
   __dirname + '/' + module.exports.app_root + '/index.js'
-];
+]
 
 // production env
 module.exports.plugins.push(
@@ -15,7 +15,7 @@ module.exports.plugins.push(
       NODE_ENV: JSON.stringify('production'),
     }
   })
-);
+)
 
 // compress the js file
 module.exports.plugins.push(
@@ -25,14 +25,14 @@ module.exports.plugins.push(
       warnings: false
     }
   })
-);
+)
 
 // export css to a separate file
 module.exports.module.loaders[1] = {
   test: /\.scss$/,
   loader: ExtractTextPlugin.extract('css!sass'),
-};
+}
 
 module.exports.plugins.push(
   new ExtractTextPlugin('../css/main.css')
-);
+)
